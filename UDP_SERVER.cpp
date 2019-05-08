146312f7b484477210c9843c5b5b1090e4be44fb
Grigoriy Mikheev <44007174+CrocodileT@users.noreply.github.com>
2019-05-09 01:19:39 +0300

    Create UDP_SERVER.cpp

---
+++ UDP_SERVER.cpp
@@ -0,0 +1,54 @@
+#include "head.h"
+std::ofstream in("msg.txt");
+
+udp_server::udp_server(boost::asio::io_service &io_service)
+    : socket_(io_service, udp::endpoint(udp::v4(), 1234))
+{
+      udp_server::start_receive();
+}
+
+void udp_server::start_receive()
+{
+      socket_.async_receive_from(
+          boost::asio::buffer(&size, sizeof(int)), remote_endpoint_,
+          boost::bind(&udp_server::handle_receive, this,
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+}
+
+void udp_server::handle_receive(const boost::system::error_code &error,
+                                std::size_t)
+{
+      if (!error)
+      {
+            s = new char[size];
+            socket_.async_receive_from(
+                boost::asio::buffer(s, size), remote_endpoint_,
+                boost::bind(&udp_server::handle_receive1, this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
+      }
+
+}
+
+void udp_server::handle_receive1(const boost::system::error_code &error,
+                                 std::size_t)
+{
+      if (!error)
+      {
+
+            recv_buffer_ = std::string(s);
+            delete[] s;
+            in << remote_endpoint_.address().to_string() + " " +  recv_buffer_<< std::endl;
+            udp_server::start_receive();
+      }
+}
+
+int main()
+{
+      boost::asio::io_service io_service;
+      udp_server server(io_service);
+      io_service.run();
+      in.close();
+      return 0;
+}
